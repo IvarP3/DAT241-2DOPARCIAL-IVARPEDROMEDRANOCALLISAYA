69dab56406c000bbcb4fc39b42e4192023970a6e
Ivar Pedro Medrano Callisaya <58529080+IvarP3@users.noreply.github.com>
2024-06-18 05:46:27 -0400

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/DatosEstudiante/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatosEstudiante
+{
+    public class Class1
+    {
+        public DataSet estudiante()
+        {
+            SqlConnection conectar = new SqlConnection();
+            conectar.ConnectionString = "server=(local);user=sa;pwd=123456;database=school";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = conectar;
+            ada.SelectCommand.CommandText = "SELECT * FROM estudiante";
+            DataSet ds = new DataSet();
+            ada.Fill(ds);
+            return ds;
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            DatosEstudiante.Class1 c1 = new DatosEstudiante.Class1();
+            this.dataGridView1.DataSource = c1.estudiante().Tables[0];
+        }
+
+    }
+}
